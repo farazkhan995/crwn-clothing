a3c3e16cc7cdac5edfc8f47c56b3d58ce8bf2d79
farazkhan995 <fazi98242@gmail.com>
2022-09-04 12:39:41 +0500

    Added Cart Icon and Cart dropdown and also integrated cart reducer and enabled it

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -4,14 +4,18 @@ import "../header/header.styles.scss";
 import { ReactComponent as Logo } from "../../assets/crown.svg";
 import { auth } from "../../firebase/firebase.utils";
 import { connect } from "react-redux";
+import CartIcon from "../cart-icon/cart-icon.component";
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 
-const Header = ({ currentUser }) => (
+const Header = ({ currentUser, hidden }) => (
 
 
     <div className="header">
+
         <Link className="logo-container" to="/">
             <Logo className="logo" />
         </Link>
+
         <div className="options">
             <Link className="option" to="/shop">
                 SHOP
@@ -26,16 +30,19 @@ const Header = ({ currentUser }) => (
             ) : (
                 <Link className="option" to="/signin">SIGN IN</Link>
             )}
-
-
-
+            <CartIcon />
         </div>
+        {
+            hidden? null : (<CartDropdown />)
+        }
+
+
     </div>
 
 )
 
-const mapStateToProps = state => (
-    {currentUser: state.user.currentUser}
+const mapStateToProps = ({user : {currentUser}, cart : {hidden}}) => (
+    {currentUser, hidden}
 );
 
 
--- src/redux/user/user.actions.js
+++ src/redux/user/user.actions.js
@@ -1,4 +1,4 @@
-import { userActionTypes } from "../user.types";
+import { userActionTypes } from "./user.types";
 
 
 export const setCurrentUser = user => ({
