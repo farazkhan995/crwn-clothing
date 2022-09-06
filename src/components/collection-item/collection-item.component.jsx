e2fa6829133f7a58e121f3e5a506490058e90edc
farazkhan995 <fazi98242@gmail.com>
2022-09-06 19:27:01 +0500

    added cart item component and linked respective redux code

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,18 +1,29 @@
 import React from "react";
 import './collection-item.styles.scss';
+import CustomButton from "../custom-button/custom-button.component";
+import { connect } from "react-redux";
+import { addItem } from '../../redux/cart/cart.actions';
+ 
 
 
+const CollectionItem = ({ item, addItem}) =>{
 
-const CollectionItem = ({ id, name, price, imageUrl }) => (
-    <div key={id} className="collection-item">
+    const { name, price, imageUrl  } = item;
+    return(
+    <div className="collection-item">
         <div style={{ backgroundImage: `url(${imageUrl})` }} className="image"></div>
         <div className="collection-footer">
             <span className="name">{name}</span>
             <span className="price">{price}</span>
         </div>
+        <CustomButton onClick={()=> addItem(item)} inverted>Add To Cart</CustomButton>
     </div>
-)
+);
+}
+
+const mapDispatchToProps = dispatch => ({
+    addItem: item => dispatch(addItem(item))
+});
 
 
-
-export default CollectionItem;
+export default connect(null, mapDispatchToProps)(CollectionItem);
--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import './custom-button.styles.scss';
 
 
-const CustomButton = ({children, isGoogleSignIn, onClick, otherProps}) => (
-    <button onClick={onClick} className= {`${isGoogleSignIn? 'google-sign-in' :  ''} custom-button`} {...otherProps}>
+const CustomButton = ({ children, isGoogleSignIn, inverted, onClick, otherProps }) => (
+    <button onClick={onClick} className={`${inverted ? 'inverted' : ''} ${isGoogleSignIn ? 'google-sign-in' : ''} custom-button`} {...otherProps}>
         {children}
     </button>
-)
+);
 
 
 export default CustomButton;
